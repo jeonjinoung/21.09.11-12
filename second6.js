5c810c736a1fd13c4620fb045aeb1922c26ad208
jeonjinoung <89626182+jeonjinoung@users.noreply.github.com>
2021-09-10 17:38:34 +0900

    Add files via upload

---
+++ second6.js
@@ -0,0 +1,30 @@
+//구조분해할당
+
+var candyMachine = {
+  status: {
+    name: "node",
+    count: 5,
+  },
+  getCandy: function () {
+    this.status.count--;
+    return this.status.count;
+  },
+};
+
+var getCandy = candyMachine.getCandy;
+var count = candyMachine.status.count;
+
+const candyMachine = {
+  status: {
+    name: "node",
+    count: 5,
+  },
+  getCandy() {
+    this.status.count--;
+    return this.status.count;
+  },
+};
+const {
+  getCandy,
+  status: { count },
+} = candyMachine;
